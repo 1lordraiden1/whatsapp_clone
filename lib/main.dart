b3be6e433c1cb824993023d1736e33405bfde802
lordraiden <lordraiden101@gmail.com>
2023-06-19 15:16:57 +0300

    Sustained

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WhatsApp',
+      title: 'WhatsApp ',
       theme: ThemeData(
         // This is the theme of your application.
         //
